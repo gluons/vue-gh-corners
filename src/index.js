ac545acd8ac1070823000fccba469d0929c73b2e
Saran Tanpituckpong <sarunta@gmail.com>
2017-06-26 21:19:07 +0700

    :sparkles: Automatically install plugin in browser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import GitHubCorners from '@/components/GitHubCorners.vue';
 
+function install(Vue) {
+	Vue.component('github-corners', GitHubCorners);
+}
+
+if ((typeof window !== 'undefined') && window.Vue) {
+	install(window.Vue);
+}
+
 export default {
-	install(Vue) {
-		Vue.component('github-corners', GitHubCorners);
-	}
+	install
 };
