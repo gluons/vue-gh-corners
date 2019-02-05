55ba51a37314440091184c4d1fe8a94e4b65e451
Saran Tanpituckpong <sarunta@gmail.com>
2019-02-05 16:33:30 +0700

    :wrench: Update Poi config

--- poi.config.js
+++ poi.config.js
@@ -1,16 +1,14 @@
+const { resolve } = require('path');
+
 module.exports = {
-	entry: './dev/main.js',
-	html: {
-		title: 'GitHub Corners',
-		description: 'GitHub Corners for Vue.'
+	entry: resolve(__dirname, './dev/main.ts'),
+	output: {
+		html: {
+			title: 'GitHub Corners'
+		}
 	},
-	presets: [
-		require('poi-preset-karma')({
-			frameworks: ['mocha', 'chai'],
-			browsers: [
-				process.env.CI ? 'FirefoxHeadless' : 'FirefoxNightlyHeadless'
-			],
-			files: ['test/**/*.js']
-		})
-	]
+	devServer: {
+		open: true
+	},
+	plugins: ['@poi/plugin-typescript']
 };
