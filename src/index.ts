f14b154c5a3689c5d2669d0730b405cd08761d69
Saran Tanpituckpong <sarunta@gmail.com>
2020-08-08 20:54:53 +0700

    💥 Use ESLint instead TSLint

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import Vue from 'vue';
 
 import GitHubCorners from './components/GitHubCorners.vue';
 
-function install(vue: typeof Vue) {
+function install(vue: typeof Vue): void {
 	vue.component('github-corners', GitHubCorners);
 }
 
-if ((typeof window !== 'undefined') && window.Vue) {
+if (typeof window !== 'undefined' && window.Vue) {
 	install(window.Vue);
 }
 
